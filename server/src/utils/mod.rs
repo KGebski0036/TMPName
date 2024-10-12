fe0dd5f19003c7bab9a1d14bb0dcc626c0fb7a40
bberni <michalb675@gmail.com>
2024-10-12 19:11:13 +0200

    format

--- server/src/utils/mod.rs
+++ server/src/utils/mod.rs
@@ -1,8 +1,6 @@
-pub mod watcher;
 pub mod builder;
+pub mod watcher;
 
 use anyhow::Result;
 
-pub fn split_at_repo(path: String) -> Result<String> { 
-    
-}
+pub fn split_at_repo(path: String) -> Result<String> {}
