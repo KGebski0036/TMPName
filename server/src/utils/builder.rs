5c90a32c353e4f9aa9787b5330178ebeacc83ba5
bberni <michalb675@gmail.com>
2024-10-12 17:20:21 +0200

    dupa

--- server/src/utils/builder.rs
+++ server/src/utils/builder.rs
@@ -2,6 +2,7 @@ use std::{fs, path::PathBuf, sync::Arc};
 use serde::{self, Deserialize, Serialize};
 use anyhow::Result;
 use thiserror::Error;
+use md5;
 
 #[derive(Error, Debug)]
 pub enum ParseError {
@@ -45,16 +46,35 @@ struct PackageMetadata {
 
 }
 
+fn to_short_entry(package_metadata: PackageMetadata, path: PathBuf) -> ShortEntry {
+    
+}
+
+
 fn parse_package_metadata(package_path: PathBuf) -> Result<PackageMetadata> {
     let metadata_path = package_path.join(PathBuf::from("metadata.json"));
     let package_metadata: PackageMetadata = match fs::read_to_string(package_path) {
         Ok(contents) => serde_json::from_str(&contents)?,
         Err(e) => {return Err(ParseError::ReadError(metadata_path, e.to_string()).into())}
     };
-    todo!()
+    Ok(package_metadata)
 }
 
 pub fn build_metadata(metadata_path: Arc<PathBuf>) -> Result<Metadata> {
+    let mut metadata: Vec<Metadata> = Vec::new();
+    fs::read_dir(metadata_path.as_path())?
+        .filter_map(Result::ok)
+        .filter(|entry| entry.path().is_dir())
+        .for_each(|entry| {
+            let path = entry.path();
+            let metadata_path = path.join("metadata.json");
 
-    todo!()
+            if metadata_path.is_file() {
+                if let Ok(pakage_metadata) = parse_package_metadata(package_path) {
+                    metadata.append();
+                }
+            } else {
+                println!("'metadata.json' not found in {}", path.display());
+            }
+        });
 }
