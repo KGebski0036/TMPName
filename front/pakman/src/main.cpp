a4a617da6042aff7b37c7573c82be21c85ed3bc6
JakeQusha <mateuszjakoniuk@outlook.com>
2024-10-13 00:46:26 +0200

    odsmierdziane

--- front/pakman/src/main.cpp
+++ front/pakman/src/main.cpp
@@ -6,12 +6,12 @@
 #include "nlohmann/json.hpp"
 
 auto main() -> int {
-    std::string url = "http://localhost:8080/get_metadata_all";  // Replace with the actual file path
-    std::string outputFile = "master.json";
+    const std::string url = "http://localhost:8080/get_metadata_all"; // Replace with the actual file path
+    const std::string outputFile = "master.json";
 
     downloadFile(url, outputFile);
 
-    std::ifstream t(outputFile);
+    const std::ifstream t(outputFile);
     std::stringstream buffer;
     buffer << t.rdbuf();
     const nlohmann::json j = nlohmann::json::parse(buffer.str());
